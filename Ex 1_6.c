88c4985ff4846910c5ff16f50bf5d4ef171f9131
SolomonS03 <solomon.s.03102007@gmail.com>
2025-09-07 19:26:57 +0530

    Add files via upload

---
+++ Ex 1_6.c
@@ -0,0 +1,11 @@
+//C program for a math software:-
+//Formula is d=(a+b)*c/5
+#include<stdio.h>
+int main(){
+    int a, b, c, d;
+    printf("Enter the values A, B and C: ");
+    scanf("%d %d %d",&a,&b,&c);
+    d = (a+b)*c/5;
+    printf("The answer for the equation: %d",d);
+    return 0;
+}
