32ec8649adf97a0c8b53ba044296431f342ddfae
SolomonS03 <solomon.s.03102007@gmail.com>
2025-09-07 19:33:30 +0530

    Update Ex 1_6.c

--- Ex 1_6.c
+++ Ex 1_6.c
@@ -3,9 +3,10 @@
 #include<stdio.h>
 int main(){
     int a, b, c, d;
-    printf("Enter the values A, B and C: ");
+    printf("Enter the values of A, B and C: ");
     scanf("%d %d %d",&a,&b,&c);
     d = (a+b)*c/5;
     printf("The answer for the equation: %d",d);
     return 0;
 }
+
