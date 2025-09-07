a2b9840e784a8166913cd5b2dc8df8f7e70a12e8
SolomonS03 <solomon.s.03102007@gmail.com>
2025-09-07 19:48:49 +0530

    Add files via upload

---
+++ Ex 2_1.1.c
@@ -0,0 +1,9 @@
+// C program to demonstrate the Auto variable:-
+#include<stdio.h>
+int main(){
+    int a = 3;
+    printf("\nThe initial value of auto variable: %d",a);
+    a = 16;
+    printf("\nThe modified value of auto variable: %d",a);
+    return 0;
+}
