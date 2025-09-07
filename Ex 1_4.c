a167efb52fdce0fe7fc2e9854afc34e8e0a93914
SolomonS03 <solomon.s.03102007@gmail.com>
2025-09-07 12:46:49 +0530

    Add files via upload

---
+++ Ex 1_4.c
@@ -0,0 +1,14 @@
+//C program to swap two values:-
+//a= 1st value, b = 2nd value from user input.
+#include<stdio.h>
+int main(){
+  int a,b,temp;
+  printf("Enter any two values: ");
+  scanf("%d %d",&a,&b);
+  temp = a;
+  a = b;
+  b = temp;
+  printf("\nThe swapped value of 'a' is: %d",a);
+  printf("\nThe swapped value of 'b' is: %d",b);
+  return 0;
+}
